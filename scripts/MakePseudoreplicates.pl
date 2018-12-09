0248f65477ae91e3623660a8641ecd933404bd0f
daniel-wells <daniel-wells@users.noreply.github.com>
2018-12-09 19:51:51 +0000

    use random number seed & don't generate bedgraph
    seed for reproducibility,
    separate bedgraph creation for modularity

--- scripts/MakePseudoreplicates.pl
+++ scripts/MakePseudoreplicates.pl
@@ -1,17 +1,16 @@
 #MakePseudoreplicates.pl
-#splits a fragment position bedfile into two pseudoreplicates
+#splits a file into two pseudoreplicates by line
 #by Nicolas Altemose
 #2015
 
 use warnings;
 use strict;
 
-my $usage = "USAGE: perl MakePseudoreplicates.pl <input file> <Proportion in Replicate 1 [default 0.5]> <chr size file [default hg19.chromsizes.tbl]> <bedtools path [default ~/bin]>";
+my $usage = "USAGE: perl MakePseudoreplicates.pl <input file> <Proportion in Replicate 1 [default 0.5]> <Integer used to seed randomness [default 42]>";
 
 my $infile;
-my $bedtoolspath = '~/bin';
 my $proprep1=0.5;
-my $chrsizefile = 'hg19.chromsizes.tbl';
+my $randomseed = 42;
 
 
 if(defined $ARGV[0]){
@@ -26,25 +25,14 @@ if(defined $ARGV[1]){
 	chomp($proprep1);
 }
 if(defined $ARGV[2]){
-	$chrsizefile = $ARGV[2];
-	chomp($chrsizefile);
+	$randomseed = $ARGV[2];
+	chomp($randomseed);
 }
-if(defined $ARGV[3]){
-	$bedtoolspath = $ARGV[3];
-	chomp($bedtoolspath);
-}
-unless(-e $chrsizefile){
-	die "ERROR: Could not find $chrsizefile in this directory\n"; 
-}
-
-
 my $outfile1 = "$infile.PR1.sorted.bed";
 my $outfile2 = "$infile.PR2.sorted.bed";
 
-my $outfile3 = "FragDepth.$infile.PR1.bed";
-my $outfile4 = "FragDepth.$infile.PR2.bed";
 
-
+srand($randomseed);
 
 my $ct1=0;
 my $ct2=0;
@@ -72,10 +60,4 @@ my $prop1 = $ct1/$tot;
 my $prop2 = $ct2/$tot;
 print "\nprinted $ct1 ($prop1) to $outfile1\n";
 print "\nprinted $ct2 ($prop2) to $outfile2\n";
-
-print "getting coverage for FragDepth files\n";
-
-system("$bedtoolspath/bedtools genomecov -bg -i $outfile1 -g $chrsizefile >$outfile3");
-system("$bedtoolspath/bedtools genomecov -bg -i $outfile2 -g $chrsizefile >$outfile4");
-
 
